db7f8abe5dfbbe59829bf03af31e2a006fda2149
martijndeheer <martijndeheer@gmail.com>
2018-05-07 23:21:32 +0200

    update

--- js/research.js
+++ js/research.js
@@ -1,10 +1,211 @@
 const research_entries = [
   {
-    "name" : 
-    "technology" : 
-    "url" : 
-    "category" :
-    "description" :
-    "img" :
+    "name" : "Face unlock iPhoneX",
+    "technology" : ,
+    "owner" : ,
+    "url" : ,
+    "category" : ["Security", "Communication"],
+    "description" : ,
+    "img" : ,
+  },
+  {
+    "name" : "Smart Speakers",
+    "technology" : ,
+    "owner" : ,
+    "url" : ,
+    "category" : ["Transport", "Eating", "Clothing", "Living", "Entertainment", "Communication", "Experience"],
+    "description" : ,
+    "img" : ,
+  },
+  {
+    "name" : "AlphaGo",
+    "technology" : ,
+    "owner" : "Google",
+    "url" : ,
+    "category" : ["Free time", "Sport"],
+    "description" : "Defeats a world champion player of the board game Go in 20016.",
+    "img" : ,
+  },
+  {
+    "name" : "Google's Eye Doctor",
+    "technology" : ,
+    "owner" : "Google",
+    "url" : ,
+    "category" : ["Health"],
+    "description" : "Hospitals in India are testing software that checks images of a person’s retina for signs of diabetic retinopathy, a condition frequently diagnosed too late to prevent vision loss.",
+    "img" : ,
+  },
+  {
+    "name" : "Autonomous Driving",
+    "technology" : ,
+    "owner" : ,
+    "url" : ,
+    "category" : [],
+    "description" : ,
+    "img" : ,
+  },
+  {
+    "name" : "Eliza",
+    "technology" : "Chatbot",
+    "owner" : "MIT",
+    "url" : ,
+    "category" : ["Health"],
+    "description" : "A personal psychotherapist in 1965.",
+    "img" : ,
+  },
+  {
+    "name" : "Meta-Dendral",
+    "technology" : ,
+    "owner" : "Stanford",
+    "url" : ,
+    "category" : ["Science"],
+    "description" : "Interprets chemical analyses and makes the first discoveries by a computer to be published in a refereed journal in 1975.",
+    "img" : ,
+  },
+  {
+    "name" : "Deep Blue",
+    "technology" : ,
+    "owner" : "IBM",
+    "url" : ,
+    "category" : [],
+    "description" : "Defeats chess world champion Garry Kasparov in 1997.",
+    "img" : ,
+  },
+  {
+    "name" : "Google Assistant",
+    "technology" : "Smart Assistant",
+    "owner" : "Google",
+    "url" : ,
+    "category" : [],
+    "description" : ,
+    "img" : ,
+  },
+  {
+    "name" : "Siri",
+    "technology" : "Smart Assistant",
+    "owner" : "Apple",
+    "url" : ,
+    "category" : [],
+    "description" : ,
+    "img" : ,
+  },
+  {
+    "name" : "Alexa",
+    "technology" : "Smart Assistant",
+    "owner" : "Amazon",
+    "url" : ,
+    "category" : [],
+    "description" : ,
+    "img" : ,
+  },
+  {
+    "name" : "Libratus",
+    "technology" : ,
+    "owner" : ,
+    "url" : ,
+    "category" : [],
+    "description" : "Out-bluffed poker kingpins.",
+    "img" : ,
+  },
+  {
+    "name" : "Blue River Technology",
+    "technology" : ,
+    "owner" : "John Deere",
+    "url" : ,
+    "category" : ["Food", "Health"],
+    "description" : "Robots capable of identifying unwanted plants, and shooting them with deadly, high-precision squirts of herbicide.",
+    "img" : ,
+  },
+  {
+    "name" : "Space Drought A.I.",
+    "technology" : ,
+    "owner" : ,
+    "url" : "https://repository.upenn.edu/cgi/viewcontent.cgi?article=1043&context=oid_papers",
+    "category" : ["Ecology", "Health", "Food"],
+    "description" : "Satellites that automatically detect drought patterns.",
+    "img" : ,
+  },
+  {
+    "name" : "Plantix",
+    "technology" : ,
+    "owner" : "PEAT GmbH",
+    "url" : "https://plantix.net/",
+    "category" : ["Ecology", "Food", "Health"],
+    "description" : "An AI-powered smartphone app that can tell a farmer what disease has crippled their crop.",
+    "img" : ,
+  },
+  {
+    "name" : "Android OS Jelly Bean",
+    "technology" : "Voice recognition neural network.",
+    "owner" : "Google",
+    "url" : ,
+    "category" : [],
+    "description" : "Google installed a voice recognition system based on a neural network resulting in users using a more natural language when speaking to the phone. In other words, they act less like they’re talking to a robot. “It really is changing the way that people behave.”",
+    "img" : ,
+  },
+  {
+    "name" : "Microsoft’s neural network-based voice processing software",
+    "technology" : "Voice Processing & Recognition",
+    "owner" : "Microsoft",
+    "url" : "https://youtu.be/Nu-nlQqFCKg",
+    "category" : ["Communication", "Experience"],
+    "description" : "Microsoft’s software simultaneously translated what he was saying and then spoke it back to the audience in Chinese. The software even adjusted its intonation to make itself sound like Rashid’s voice.",
+    "img" : ,
+  },
+  {
+    "name" : "Cat Identifier",
+    "technology" : "Image Processing & Recognition",
+    "owner" : "Google",
+    "url" : "https://www.nytimes.com/2012/06/26/technology/in-a-big-network-of-computers-evidence-of-machine-learning.html?pagewanted=all&_r=0",
+    "category" : ["Science"],
+    "description" : "The neural network taught itself to recognize cats.",
+    "img" : ,
+  },
+  {
+    "name" : "Image recognition A.I.",
+    "technology" : "Deep-Learning",
+    "owner" : "Google & Stanford",
+    "url" : ,
+    "category" : [],
+    "description" : "A.I. correctly captioned a photo of “a group of young people playing a game of Frisbee”.",
+    "img" : ,
+  },
+  {
+    "name" : "DeepText",
+    "technology" : "Machine learning & word embedding",
+    "owner" : "Instagram",
+    "url" : "https://www.wired.com/2017/08/instagram-kevin-systrom-wants-to-clean-up-the-internet/",
+    "category" : [],
+    "description" : "Trying to clean up the internet, or at least part of it.",
+    "img" : ,
+  },
+  {
+    "name" : ,
+    "technology" : ,
+    "owner" : ,
+    "url" : ,
+    "category" : [],
+    "description" : ,
+    "img" : ,
   },
 ]
+
+"### Categories:"
+"Transport"
+"Food"
+"Clothing"
+"Living"
+"Working"
+"Entertainment"
+"Politics"
+"News"
+"Health"
+"Sport"
+"Free time"
+"Finance"
+"Experience"
+"Security"
+"Communication"
+"Science"
+"Ecology"
+
